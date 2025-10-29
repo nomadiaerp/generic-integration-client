e09d029853db97037d9d028ecd544621b747dfef
Nomadia <nomadia.erp@gmail.com>
2025-10-29 14:23:57 -0300

    Update __init__.py

--- generic-integration-client/__init__.py
+++ generic-integration-client/__init__.py
@@ -1,1 +1,5 @@
+from .client import GenericIntegrationClient
+from .exceptions import AuthenticationError, IntegrationError
+
+__all__ = ["GenericIntegrationClient", "AuthenticationError", "IntegrationError"]
 
